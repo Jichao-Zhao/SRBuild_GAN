2fd323e3e7275f14456f72bfcf5542cc0615eea0
478984342@qq.com <478984342@qq.com>
2020-09-12 09:12:03 +0800

    Evaluate upgrade skimage function

--- Evaluate.py
+++ Evaluate.py
@@ -4,16 +4,16 @@
 # 注意：图像通道数可以任意，但是对比图像必须要保持相同
 
 import cv2
-from skimage.measure import compare_mse
-from skimage.measure import compare_psnr
-from skimage.measure import compare_ssim
+from skimage.metrics import mean_squared_error
+from skimage.metrics import peak_signal_noise_ratio
+from skimage.metrics import structural_similarity
 
 img1 = cv2.imread('imgPred0.png')
 img2 = cv2.imread('imgPred01.png')
 
-MSE = compare_mse(img1, img2)
-PSNR = compare_psnr(img1, img2)
-SSIM = compare_ssim(img1, img2, multichannel=True)
+MSE = mean_squared_error(img1, img2)
+PSNR = peak_signal_noise_ratio(img1, img2)
+SSIM = structural_similarity(img1, img2, multichannel=True)
 
 print('MSE: ', MSE)
 print('PSNR: ', PSNR)
